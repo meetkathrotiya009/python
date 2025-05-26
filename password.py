b5be732d6d5a36dcb2781e9c56a1006171df1962
meetkathrotiya009 <meetkathrotiya176@gmail.com>
2025-05-26 12:09:43 +0530

    password checker

---
+++ password.py
@@ -0,0 +1,10 @@
+user_v=input("enter the password:")
+if len(user_v)<8:
+    print("password to shooot lessthen to 8")
+    elif not any (char.isdigit9()for char in user_v):
+        print("do not use character")
+    elif not any(char in "!@#$" for char in user_v):
+        print("the speicl symbol")
+        else:
+            print ("corect the password")
+
